718ffc1e61e420c29d998d8540b581f5266cac9c
nhatphongcgp <phongnguyen64686969@gmail.com>
2021-06-28 17:15:38 +0700

    second commit

--- lib/app/jobs_page.dart
+++ lib/app/jobs_page.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_app/app/common_widgets/show_alert_dialogs.dart';
+import 'package:time_tracker_app/app/common_widgets/show_exception_alert_dialog.dart';
+import 'package:time_tracker_app/app/model/jobs_models.dart';
 import 'package:time_tracker_app/services/auth.dart';
 import 'package:time_tracker_app/services/database.dart';
 
@@ -27,9 +30,17 @@ class JobsPage extends StatelessWidget {
     }
   }
 
-  void submit(BuildContext context) async {
-    final database = Provider.of<Database>(context, listen: false);
-    await database.createJob({'name': 'Blogging', 'ratePerHour': '10'});
+  Future<void> _createJob(BuildContext context) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+    } on FirebaseException catch (e) {
+      showExceptionAlertDialog(
+        context,
+        title: 'Operation failed',
+        exception: e,
+      );
+    }
   }
 
   @override
@@ -46,8 +57,32 @@ class JobsPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => submit(context),
+        onPressed: () => _createJob(context),
       ),
+      body: _buildContents(context),
     );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<List<Job>>(
+        stream: database.jobsStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final jobs = snapshot.data;
+            final children = jobs.map((job) => Text(job.name)).toList();
+            return ListView(
+              children: children,
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Some error occured'),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,16 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:time_tracker_app/app/model/jobs_models.dart';
+import 'package:time_tracker_app/services/api_auth.dart';
 
 abstract class Database {
-  Future<void> createJob(Map<String, dynamic> jobData);
+  Future<void> createJob(Job job);
+  Stream<List<Job>> jobsStream();
+  // void readJobs();
 }
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
-  Future<void> createJob(Map<String, dynamic> jobData) async {
-    final String path = 'users/$uid/jobs/job_abc';
-    final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(jobData);
+  Future<void> createJob(Job job) async {
+    await _setData(
+      path: APIPath.job(uid, 'job_abc'),
+      data: job.toMap(),
+    );
+  }
+
+  // void readJobs() {
+  //   final path = APIPath.jobs(uid);
+  //   final reference = FirebaseFirestore.instance.collection(path);
+  //   final snapshots = reference.snapshots();
+  //   snapshots.listen((snapshot) {
+  //     snapshot.docs.forEach((element) {
+  //       print(element.data());
+  //     });
+  //   });
+  // }
+  Stream<List<Job>> jobsStream() {
+    final path = APIPath.jobs(uid);
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => snapshot.docs.map((snapshot) {
+          final data = snapshot.data();
+          return data != null
+              ? Job(name: data['name'], ratePerHour: data['ratePerHour'])
+              : null;
+        }).toList());
+  }
+
+  Future<void> _setData({String path, Map<String, dynamic> data}) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('path:$path');
+    await reference.set(data);
   }
 }
